20ba68cc6e37609f4573093e0b7ab1fd2fcc3fd3
dahmouni <a.dahmouni_etu@enset-media.ac.ma>
2021-06-05 11:18:15 +0100

    question 14

--- dahmouni/seeders/20210605092950-users-create.js
+++ dahmouni/seeders/20210605092950-users-create.js
@@ -1,16 +1,31 @@
 'use strict';
-
+var faker = require('faker');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+     var data = [];
+
+     for(var i=0; i<20; ++i) 
+     {
+     
+     data.push({
+     
+     username: faker.internet.userName(),
+     
+     email: faker.internet.email(),
+     
+     password: faker.internet.password(),
+     
+     role: i == 0 ? 'admin': (i%2 == 0 ? 'author' : 'guest'),
+     
+     createdAt: new Date(),
+     
+     updatedAt: new Date()
+     
+     });
+     
+     }
+     
+     await queryInterface.bulkInsert('Users', data, {});
   },
 
   down: async (queryInterface, Sequelize) => {
