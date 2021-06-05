924235deeb0840093e3481201e903a5f753ad3e0
dahmouni <a.dahmouni_etu@enset-media.ac.ma>
2021-06-05 10:32:26 +0100

    question 11 et 12

--- dahmouni/migrations/20210605074954-create-user.js
+++ dahmouni/migrations/20210605074954-create-user.js
@@ -13,7 +13,7 @@ module.exports = {
         allowNull:false
       },
       email: {
-        type:DataTypes.STRING,
+        type:Sequelize.STRING,
         unique:true,
         allowNull:false
       },
