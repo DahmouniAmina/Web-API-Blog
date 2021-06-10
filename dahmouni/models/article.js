6613db906ec13644bbc7becd77986f091c702219
dahmouni <a.dahmouni_etu@enset-media.ac.ma>
2021-06-10 23:54:43 +0100

    question 19

--- dahmouni/models/article.js
+++ dahmouni/models/article.js
@@ -1,4 +1,5 @@
 'use strict';
+const { unique } = require('faker');
 const {
   Model
 } = require('sequelize');
@@ -11,15 +12,25 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Article.belongsTo(models.User,{foreignKey: 'userId'})
+      Article.hasMany(models.Comment,{foreignKey: 'articleId'})
+      Article.belongsToMany(models.Tag, {through: 'ArticleTags'})
     }
   };
   Article.init({
-    title: DataTypes.STRING,
+    title: {
+      type: DataTypes.STRING,
+      unique:true
+    },
     content: DataTypes.TEXT,
-    published: DataTypes.BOOLEAN
+    published: {
+      type: DataTypes.BOOLEAN,
+      defaultValue:true
+    }
   }, {
     sequelize,
     modelName: 'Article',
   });
   return Article;
 };
+
